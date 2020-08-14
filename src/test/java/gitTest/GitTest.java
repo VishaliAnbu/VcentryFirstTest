94ac0f448adcf77ca45dabf800b8180ac0218aa4
VishaliAnbu <vishalipriya77@gmail.com>
2020-08-14 09:27:04 +0530

    Allfilecommit

---
+++ src/test/java/gitTest/GitTest.java
@@ -0,0 +1,19 @@
+package gitTest;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.Test;
+
+public class GitTest {
+
+	@Test
+	public void loginTest(){
+		WebDriver wd = new FirefoxDriver();
+		wd.get("http://www.google.com");
+		System.out.println(wd.getTitle());
+		
+		wd.findElement(By.name("q")).sendKeys("chennai");
+		wd.findElement(By.name("q")).submit();
+	}
+}
